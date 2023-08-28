c691878c732a89bcdcde6305ac250623a39b8dc9
dylanb-101 <dylanbarrett360@gmail.com>
2023-08-28 14:54:12 -0400

    added distance selector
    added a distance selector to the home page that lets the user input how many miles/km they want to run - not functional yet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,10 @@
 <script>
     import "../app.css";
-    
+    import Nav from "$lib/nav.svelte";
 </script>
 
 
 
-<div id="nav-bar-container" class="max-w-5xl md:pl-8 md:pr-8" style="margin: 1rem auto;"> 
-
-    <div id="nav-bar-flex" class="md:flex -mt-3 -ml-3 -mr-3 last:-mb-3"> 
-
-        <div class="block basis-0 grow shrink p-3 text-center md:text-start ">
-
-            <a href="/" class="inline-flex md:inline">
-                <img src="../dice.svg" alt="logo" class="float-left h-9">
-                <h1 class=" text-3xl font-semibold">Roll2Run</h1>
-            </a>
-        </div>
-
-        <div class="block p-0 basis-0 grow shrink md:p-3 md:text-right self-center text-center">
-            
-            <a href="/login" class="inline-flex ml-0 md:inline md:ml-8 font-bold transition ease-in-out text-gray-500 hover:text-gray-950 hover:scale-100">
-                Login
-            </a>
-            <a href="/about" class="inline-flex ml-6 md:inline md:ml-8 font-bold transition ease-in-out text-gray-500 hover:text-gray-950 ">
-                About
-            </a>
-
-
-        </div>
-        
-    </div>
-</div>
+<Nav></Nav>
 
 <slot></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,53 @@
+<script>
+
+    let measurment = "miles";
+
+    const toggleMeasurment = () => {
+        if(measurment === "miles"){
+            measurment = "km";
+        } else measurment = "miles";
+    }
+
+</script>
+
+
 <div class="bg-blue-400 w-screen h-screen pt-4 pr-4">
 
     <div class=" bg-white mr-4 ml-4 p-4 grid grid-cols-8 grid-rows-5 gap-x-4 gap-y-2">
         
+        <!-- search bar header -->
         <div class="row-span-1 col-span-3 text-lg">
-            <p>Enter Starting Address</p>
+            <p>Starting Address</p>
         </div>
+
+        <!-- address search bar -->
         <div class="row-start-2 row-end-3 col-span-3">
             <input type="text" class="border-2 border-gray-400 rounded-md w-full hover:border-gray-500 transition ease-in-out bg-gray-100 text-black placeholder:text-gray-500" placeholder="Starting Address">
         </div>
 
+        <!-- my location button -->
         <div class="row-start-2 row-end-3 col-start-4 col-end-5">
             <button class="bg-zinc-500 border-2 rounded-md m-0 h-7 p-0.5">
                 <img src="./my_location.svg" alt="My Location" class=" h-full">
             </button>
+        </div>
+
+        <!-- distance header -->
+        <div class="row-start-3 row-end-4 col-span-1 flex">
+            <p class="self-end">Distance</p>
+        </div>
+
+        <!-- distance input -->
+        <div class="row-start-4 row-end-5 col-span-1">
+            <input type="number" class="border-2 border-gray-400 rounded-md w-full hover:border-gray-500 transition ease-in-out bg-gray-100 text-black placeholder:text-gray-500" value="3">
+        </div>
+
+        <!-- distance measurment selector -->
+        <div class="row-start-4 row-end-5 col-start-2 col-end-3">
+            <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <p class="hover:text-neutral-500 transition ease-in-out hover:cursor-pointer font-medium w-fit p-1" on:click={toggleMeasurment}>{measurment}</p>
+
         </div>
 
     </div>
